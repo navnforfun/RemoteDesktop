1641561a1e147457a92c7c04774aaec9501567ae
navnforfun@gmail.com <navnforfun@gmail.com>
2024-02-08 16:07:28 +0700

    Start project

---
+++ src/ex16_project_remotedesktop/Ex16_Project_RemoteDesktop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex16_project_remotedesktop;
+
+/**
+ *
+ * @author ngocanh
+ */
+public class Ex16_Project_RemoteDesktop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
